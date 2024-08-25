ba26e31e01cf5a2f2aa99029ad06124664892aa1
zhangyunzhi-web <318866560@qq.com>
2024-08-26 01:23:12 +0800

    chored:修改代码顺序

--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -1,4 +1,3 @@
-
 /*
  * @Author: wwhth zhangyunzhi@vrvmail.com.cn
  * @Date: 2024-08-23 11:24:03
@@ -7,14 +6,20 @@
  * @FilePath: /vue-source/packages/runtime-dom/src/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-export * from '@vue/reactivity'
-import { nodeOps } from './nodeOps'
-import patchProp from './patchProp'
+import { nodeOps } from "./nodeOps";
+import patchProp from "./patchProp";
 
-const renderOptions = Object.assign({ patchProp }, nodeOps)
-function createRenderer(option) {
+const renderOptions = Object.assign({ patchProp }, nodeOps);
+export { renderOptions };
+export * from "@vue/reactivity";
+// function createRenderer(option) {
+//   const {
+//     createElement: hostCreateElement,
+//     patchProp: hostPatchProp,
+//     insert: hostInsert,
+//     remove: hostRemove,
+//     setElementText: hostSetElementText,
+//   } = option;
+// }
 
-    const { createElement: hostCreateElement, patchProp: hostPatchProp, insert: hostInsert, remove: hostRemove, setElementText: hostSetElementText } = option
-}
-
-createRenderer(renderOptions)
+// createRenderer(renderOptions);
